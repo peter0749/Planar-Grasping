fa612dbdf436f45e40c10166642eabc1f569853b
Your Name <you@example.com>
2019-07-10 19:16:10 +0800

    preprocessing

---
+++ preprocess.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd `dirname "$0"`
+cd DATA
+mkdir -p "./raw_data/fg"
+mkdir -p "./raw_data/bg"
+
+for ((i=1; i<=10; ++i)); do
+    tarname=`printf "data%02d.tar.gz" $i`
+    tar -zxvf "$tarname" -C "./raw_data/fg" --strip-components=1
+done
+
+unzip backgrounds.zip -d "./raw_data/bg"
+unzip processedData.zip -d "./processedData"
+uniq "./processedData/z.txt" > "./processedData/z_uniq.txt"
+
+cd ..
+
+sync && python preprocess_id.py
+
